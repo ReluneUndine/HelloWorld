0919f2ed515cb3f0b82617823e7de8a5636e0348
1645286 <1645286@LAPTOP-THC5M6M0>
2023-03-26 12:31:53 +0300

    Добавьте файлы проекта.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {/// <summary>
+    /// Точка входа! Код, который выполняется в первую очередь :)
+    /// </summary>
+    /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            /// Очень важно писать текст именно в двойных кавычках и не забывать точку после Console, иначе будут ошибки CS1011, CS1012!
+            Console.WriteLine("Hello World !!!");
+            Console.ReadKey();
+            
+        }
+    }
+}
